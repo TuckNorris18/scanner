4ed8a4dbf4926a5aaf22d7e8d3d144b030ffe975
TuckNorris18 <63086220+TuckNorris18@users.noreply.github.com>
2021-10-21 16:25:28 -0500

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -7,12 +7,17 @@ using namespace std;
 bool isString(char c);
 bool isSpace(char c);
 bool isPeriod(char c);
+bool isComment(string str);
+bool isRead(string str);
+bool isWrite(string str);
+string isToken(string str);
 
-void getTokens(vector<string> &arr) 
+void scan(vector<string> &arr) 
 {
     ifstream in("Text.txt");
     char c;
     string str = "";
+    string temp = "";
     
     //error reading file
     if (!in.eof() && in.fail())
@@ -25,37 +30,52 @@ void getTokens(vector<string> &arr)
     if(in.is_open()) {
         while (in.get(c)) 
         {
-            cout << c << '\n';
+            
             if (isString(c))
             {
                 string s(1, c);
                 str.append(s);
 
-                //cout << s << '\n';
+                //cout << isComment(str) << endl;
+
+                if (isComment(str) == true)
+                {
+                    cout << "Comment detected" << endl;
+                    while (c != '/')
+                    {
+                        cout << c << endl;
+
+                        in.get(c);
+                    }
+                }
+
+                
             }
             if(isSpace(c) || isPeriod(c))
             {
-                //cout << str << '\n';
+                cout << "Adding: " << str << endl;
                 arr.push_back(str);
                 str = "";
             }
-            //cout << str << '\n';
+            if (c == '\n')
+            {
+                //cout << "End of line" << endl;
+            }
+            
         }
         
     }
-  
-    //cout << str;
 
     in.close();
 }
 bool isString(char c)
 {
     int temp = (int)c;
-    if (temp >= 41 && temp <= 132)
+    if (temp >= 65 && temp <= 90)
     {
         return true;
     }
-    if (temp >= 141 && temp <= 172)
+    if (temp >= 97 && temp <= 122)
     {
         return true;
     }
@@ -78,19 +98,111 @@ bool isPeriod(char c)
     }
     return false;
 }
+bool isComment(string str)
+{
+    if (str == "/*")
+    {
+        return true;
+    }
+    return false;
+}
+bool isRead(string str)
+{
+    if (str == "read")
+    {
+        return true;
+    }
+    return false;
+}
+bool isWrite(string str)
+{
+    if (str == "write")
+    {
+        return true;
+    }
+    return false;
+}
+bool isID(string str)
+{
+    bool flag = true;
+    for (int i = 0; i < str.length(); i++)
+    {
+        
+        if (!isString(str[i]))
+        {
+            
+            flag = false;
+        }
+    }
+    return flag;
+}
+bool isNum(string str)
+{
+    bool flag = true;
+    for (int i = 0; i < str.length(); i++)
+    {
+        if (!((int)str[i] >= 48 && (int)str[i] <= 57))
+        {
+            flag = false;
+        }
+    }
+
+    return flag;
+}
+bool isAssign(string str)
+{
+    bool flag = false;
+    for (int i = 0; i < str.length(); i++)
+    {
+        if ((int)str[i] == 58)
+        {
+            if ((int)str[i + 1] == 61)
+            {
+                return true;
+            }
+        }
+    }
+    return flag;
+}
+string isToken(string str)
+{
+    if (isWrite(str))
+    {
+        return "write";
+    }
+    if (isRead(str))
+    {
+        return "read";
+    }
+    if (isID(str))
+    {
+        return "ID";
+    }
+    if (isNum(str))
+    {
+        return "num";
+    }
+}
 int main() {
     vector<string> arr;
     
-    getTokens(arr);
+    scan(arr);
 
-    
+    /*
     for (auto i = arr.begin(); i != arr.end(); ++i)
     {
         cout << *i << " ";
     }
-        
+    */
+
     
-    cout << '\n' << arr.size();
+    for (int i = 0; i < arr.size(); i++)
+    {
+        cout << arr[i] << endl;
+    }
+    
+
+    //cout << isToken("5") << endl;
 
     return 0;
 }
